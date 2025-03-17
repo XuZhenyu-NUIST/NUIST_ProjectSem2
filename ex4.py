4d7f4439dc78b7f1518b6b7ee67b87c7ed0abd0a
xzy <715660750@qq.com>
2025-03-17 16:54:46 +0800

    Add ex4.py

---
+++ ex4.py
@@ -0,0 +1,7 @@
+a = int(input("Enter your age: "))
+if a<=19:
+  print(" You have the qualify for student discounts")
+elif 20<a<54:
+  print("No age discounts")
+else:
+  print(" You can receive senior discounts")
